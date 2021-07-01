3c0baa8617c231119970d07d82ee4a50049ac514
eunyoung <eun970923@naver.com>
2021-07-01 23:37:47 +0900

    [feat]UIcolor, font extension 추가

--- Keepin_iOS/Keepin_iOS/Configuration/Extension/UIColor.swift
+++ Keepin_iOS/Keepin_iOS/Configuration/Extension/UIColor.swift
@@ -23,4 +23,56 @@ extension UIColor {
     // MARK: 메인 테마 색 또는 자주 쓰는 색을 정의
     // ex. label.textColor = .mainOrange
     //ex. class var mainPurple: UIColor { UIColor(hex: 0x5f0080) }
+    
+    @nonobjc class var keepinBlack: UIColor {
+       return UIColor(white: 52.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGray: UIColor {
+       return UIColor(red: 243.0 / 255.0, green: 245.0 / 255.0, blue: 248.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGray3: UIColor {
+       return UIColor(white: 204.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGray4: UIColor {
+       return UIColor(white: 165.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGray2: UIColor {
+       return UIColor(white: 230.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGreen: UIColor {
+       return UIColor(red: 21.0 / 255.0, green: 189.0 / 255.0, blue: 111.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGray1: UIColor {
+       return UIColor(white: 249.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var keepinGray5: UIColor {
+       return UIColor(white: 87.0 / 255.0, alpha: 1.0)
+     }
+
+     @nonobjc class var seaweed: UIColor {
+       return UIColor(red: 21.0 / 255.0, green: 189.0 / 255.0, blue: 111.0 / 255.0, alpha: 1.0)
+     }
+    
+    @nonobjc class var keepinGray250: UIColor {
+      return UIColor(white: 230.0 / 255.0, alpha: 0.5)
+    }
+    
+    @nonobjc class var black20: UIColor {
+       return UIColor(white: 0.0, alpha: 0.2)
+     }
+    
+    @nonobjc class var ice: UIColor {
+      return UIColor(red: 217.0 / 255.0, green: 245.0 / 255.0, blue: 232.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var salmon: UIColor {
+      return UIColor(red: 241.0 / 255.0, green: 114.0 / 255.0, blue: 114.0 / 255.0, alpha: 1.0)
+    }
 }
---
+++ Keepin_iOS/Keepin_iOS/Configuration/Extension/UIFont.swift
@@ -0,0 +1,177 @@
+//
+//  UIFont.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/01.
+//
+
+import Foundation
+
+extension UIFont {
+    
+    class var textStyle2: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 22.0)!
+      }
+
+      class var textStyle36: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 20.0)!
+      }
+
+      class var textStyle33: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 20.0)!
+      }
+
+      class var textStyle5: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 20.0)!
+      }
+
+      class var textStyle39: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 18.0)!
+      }
+
+      class var textStyle11: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 18.0)!
+      }
+
+      class var textStyle14: UIFont {
+        return UIFont(name: "NotoSans-Bold", size: 16.0)!
+      }
+
+      class var textStyle38: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 16.0)!
+      }
+
+      class var textStyle19: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 16.0)!
+      }
+
+      class var textStyle15: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 16.0)!
+      }
+
+      class var textStyle30: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 16.0)!
+      }
+
+      class var textStyle24: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 16.0)!
+      }
+
+      class var textStyle: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 16.0)!
+      }
+
+      class var textStyle32: UIFont {
+        return UIFont(name: "NotoSans-Regular", size: 16.0)!
+      }
+
+      class var textStyle27: UIFont {
+        return UIFont(name: "NotoSans-Regular", size: 16.0)!
+      }
+
+      class var textStyle13: UIFont {
+        return UIFont(name: "NotoSans-Regular", size: 16.0)!
+      }
+
+      class var textStyle34: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 16.0)!
+      }
+
+      class var textStyle29: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 16.0)!
+      }
+
+      class var textStyle23: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 16.0)!
+      }
+
+      class var textStyle20: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 16.0)!
+      }
+
+      class var textStyle16: UIFont {
+        return UIFont(name: "GmarketSansTTF-Light", size: 16.0)!
+      }
+
+      class var textStyle41: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 14.0)!
+      }
+
+      class var textStyle28: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 14.0)!
+      }
+
+      class var textStyle7: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 14.0)!
+      }
+
+      class var textStyle6: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Bold", size: 14.0)!
+      }
+
+      class var textStyle3: UIFont {
+        return UIFont(name: "GmarketSansTTF-Medium", size: 14.0)!
+      }
+
+      class var textStyle10: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Medium", size: 14.0)!
+      }
+
+      class var textStyle8: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Medium", size: 14.0)!
+      }
+
+      class var textStyle37: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle35: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle31: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle26: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle25: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle22: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle21: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle18: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle17: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 14.0)!
+      }
+
+      class var textStyle40: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 12.0)!
+      }
+
+      class var textStyle12: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 12.0)!
+      }
+
+      class var textStyle9: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 12.0)!
+      }
+
+      class var textStyle4: UIFont {
+        return UIFont(name: "NotoSansCJKKR-Regular", size: 12.0)!
+      }
+
+
+}
