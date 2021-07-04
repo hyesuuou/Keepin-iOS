c45a0361bb853b7b1f06e249437248e73419eb08
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-04 22:25:19 +0900

    [fix] #5 홈 welcomelabel 줄 간격 수정

--- Keepin_iOS/Keepin_iOS/Sources/Home/Cell/HomeTopTVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/Home/Cell/HomeTopTVC.swift
@@ -19,13 +19,16 @@ class HomeTopTVC: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        welcomeLabel.text = "\(username)님\n키핀은 아름다워요."
+        welcomeLabel.text = "\(username)님\n키핀은 아름다워요"
         welcomeLabel.font = UIFont.GmarketSansTTF(.medium, size: 22)
         let attributedStr = NSMutableAttributedString(string: welcomeLabel.text!)
-        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 8
         attributedStr.addAttribute(.foregroundColor, value: UIColor.keepinGreen , range: (welcomeLabel.text! as NSString).range(of: "\(username)"))
-        
+        attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedStr.length))
         welcomeLabel.attributedText = attributedStr
+    
+        
         
         messageLabel.text = "PM이 탕수육 사줬다~~"
         messageLabel.font = UIFont.GmarketSansTTF(.medium, size: 14)
