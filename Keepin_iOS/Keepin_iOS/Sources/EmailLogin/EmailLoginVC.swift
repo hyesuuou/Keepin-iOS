d83159155b95a71250167cd695c5533ad87b59f6
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-06 14:59:23 +0900

    [feat] #15 textField 라운드 + 앞에 여백

--- Keepin_iOS/Keepin_iOS/Sources/EmailLogin/EmailLoginVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/EmailLogin/EmailLoginVC.swift
@@ -9,17 +9,39 @@ import UIKit
 
 class EmailLoginVC: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loginButton.backgroundColor = .keepinGray1
         loginButton.tintColor = .keepinGray3
         loginButton.layer.cornerRadius = 26
-        // Do any additional setup after loading the view.
+        
+        setTextFieldUI(tf: idTextField)
+        setTextFieldUI(tf: pwTextField)
+        
+        
     }
 
+    func setTextFieldUI(tf : UITextField){
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor.keepinGray3.cgColor
+        tf.layer.cornerRadius = 12
+        tf.addLeftPadding()
+    }
 
     
 
+}
+
+
+extension UITextField {
+  func addLeftPadding() {
+    let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 12.9, height: self.frame.height))
+    self.leftView = paddingView
+    self.leftViewMode = ViewMode.always
+  }
 }
