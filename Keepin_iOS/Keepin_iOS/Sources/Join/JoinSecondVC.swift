b39966efe8b61ac37094644804fa22b0e5952adc
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-06 17:07:19 +0900

    [feat] #18 회원가입 파일 추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/Join/JoinSecondVC.swift
@@ -0,0 +1,29 @@
+//
+//  JoinSecondVC.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/06.
+//
+
+import UIKit
+
+class JoinSecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
