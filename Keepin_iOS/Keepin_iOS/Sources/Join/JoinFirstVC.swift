1d1642518104604ec58dfe758caf0a32d2fc6c48
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-07 01:11:06 +0900

    [feat] #18 textfield 밑줄 색 바뀌도록

--- Keepin_iOS/Keepin_iOS/Sources/Join/JoinFirstVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/Join/JoinFirstVC.swift
@@ -12,17 +12,61 @@ class JoinFirstVC: UIViewController {
     
     @IBOutlet var titleLabel: [UILabel]!
     
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var pwOKTextField: UITextField!
+    
+    @IBOutlet var underlineView: [UIView]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUI()
+        
+        idTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        pwTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        pwOKTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    func setUI(){
         titleLabel[0].font = UIFont.GmarketSansTTF(.medium, size: 20)
         titleLabel[1].font = UIFont.GmarketSansTTF(.medium, size: 16)
         titleLabel[2].font = UIFont.GmarketSansTTF(.medium, size: 16)
         titleLabel[3].font = UIFont.GmarketSansTTF(.medium, size: 16)
         
+        idTextField.font = UIFont.NotoSans(.regular, size: 16)
+        pwTextField.font = UIFont.NotoSans(.regular, size: 16)
+        pwOKTextField.font = UIFont.NotoSans(.regular, size: 16)
+        
+        underlineView[0].backgroundColor = .keepinGray3
+        underlineView[1].backgroundColor = .keepinGray3
+        underlineView[2].backgroundColor = .keepinGray3
     }
     
-    
+    @objc func textFieldDidChange(_ sender: Any?) {
+        
+
+        if idTextField.text != "" {
+            underlineView[0].backgroundColor = .keepinBlack
+        }
+        else if idTextField.text == "" {
+            underlineView[0].backgroundColor = .keepinGray3
+        }
+        
+        if pwTextField.text != "" {
+            underlineView[1].backgroundColor = .keepinBlack
+        }
+        else if idTextField.text == "" {
+            underlineView[1].backgroundColor = .keepinGray3
+        }
+        
+        if pwOKTextField.text != "" {
+            underlineView[2].backgroundColor = .keepinBlack
+        }
+        else if pwOKTextField.text == "" {
+            underlineView[2].backgroundColor = .keepinGray3
+        }
+    }
 
 
     
