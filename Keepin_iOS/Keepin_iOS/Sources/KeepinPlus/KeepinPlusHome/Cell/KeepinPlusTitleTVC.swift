c472fa49c0f6075c463a530db3fce0d9e7c71076
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-09 06:02:42 +0900

    [feat] #36 타이틀 셀 파일 추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusTitleTVC.swift
@@ -0,0 +1,23 @@
+//
+//  KeepinPlusTitleTVC.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/09.
+//
+
+import UIKit
+
+class KeepinPlusTitleTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
