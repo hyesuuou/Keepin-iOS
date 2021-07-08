689eae5d3832d75ec73e946fada045666f8bddb7
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-09 07:02:28 +0900

    [feat] #36 타이틀 추가

--- Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusTitleTVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusTitleTVC.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class KeepinPlusTitleTVC: UITableViewCell {
+    
+    public static let identifier = "KeepinPlusTitleTVC"
 
+    @IBOutlet weak var importantImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUI()
         // Initialization code
     }
 
@@ -18,6 +25,26 @@ class KeepinPlusTitleTVC: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func setData(title : String, subtitle: String, image : Bool) {
+        
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+        if image == true {
+            importantImageView.isHidden = false
+        }
+        else {
+            importantImageView.isHidden = true
+        }
+        
+    }
+    
+    func setUI(){
+        titleLabel.font = UIFont.GmarketSansTTF(.medium, size: 16)
+        subtitleLabel.font = UIFont.GmarketSansTTF(.light, size: 16)
+        subtitleLabel.textColor = .keepinGray4
+        
     }
     
 }
