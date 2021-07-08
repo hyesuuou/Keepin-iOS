b9bc442fd0ce860619d60309268433293e51f54c
Yi Joon Choi <69078056+dlwns33@users.noreply.github.com>
2021-07-08 21:19:58 +0900

    [feat] #21 카테고리 파일추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/PresentMoa/Search/AfterCate/AfterCatVC.swift
@@ -0,0 +1,29 @@
+//
+//  AfterCatVC.swift
+//  Keepin_iOS
+//
+//  Created by Yi Joon Choi on 2021/07/08.
+//
+
+import UIKit
+
+class AfterCatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
