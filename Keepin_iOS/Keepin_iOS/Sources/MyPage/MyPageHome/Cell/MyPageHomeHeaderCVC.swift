b382c7a9f204cc0dc14a7fa5cc3e99fd10455e52
eunyoung <eun970923@naver.com>
2021-07-09 22:03:45 +0900

    [feat] #7 마이페이지 메인뷰 파일 추가 수정

---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageHome/Cell/MyPageHomeHeaderCVC.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageHomeHeaderCVC.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/09.
+//
+
+import UIKit
+
+class MyPageHomeHeaderCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
