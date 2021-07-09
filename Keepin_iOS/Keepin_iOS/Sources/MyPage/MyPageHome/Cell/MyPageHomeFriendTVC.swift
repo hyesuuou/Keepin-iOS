eb762bc3d227b1833a8fe24221dcf947edf16ca2
eunyoung <eun970923@naver.com>
2021-07-09 21:29:01 +0900

    [feat] #7 마이페이지 메인뷰 수정 파일 추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageHome/Cell/MyPageHomeFriendTVC.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageHomeFriendTVC.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/09.
+//
+
+import UIKit
+
+class MyPageHomeFriendTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
