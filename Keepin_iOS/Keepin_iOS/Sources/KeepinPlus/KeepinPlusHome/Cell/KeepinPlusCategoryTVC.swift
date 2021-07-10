6e7956704731130dc028b61d7150d702090b41e9
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-10 16:38:21 +0900

    [feat] #36 셀파일추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusCategoryTVC.swift
@@ -0,0 +1,23 @@
+//
+//  KeepinPlusCategoryTVC.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/10.
+//
+
+import UIKit
+
+class KeepinPlusCategoryTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
