f4aae3943f1ad26655d58015b4821d696ee04c2d
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-12 15:29:14 +0900

    [fix] #79 키핀하기 상단 여백 줌

--- Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusBlankTVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusBlankTVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class KeepinPlusBlankTVC: UITableViewCell {
+    
+    public static let identifier = "KeepinPlusBlankTVC"
 
     override func awakeFromNib() {
         super.awakeFromNib()
