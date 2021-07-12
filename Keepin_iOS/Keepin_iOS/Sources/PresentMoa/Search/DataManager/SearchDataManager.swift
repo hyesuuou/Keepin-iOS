9a6c38b80767d42de694eb565b24f3407f0151e6
Yi Joon Choi <69078056+dlwns33@users.noreply.github.com>
2021-07-12 19:32:51 +0900

    #88 검색 검색어 서버통신

--- Keepin_iOS/Keepin_iOS/Sources/PresentMoa/Search/DataManager/SearchDataManager.swift
+++ Keepin_iOS/Keepin_iOS/Sources/PresentMoa/Search/DataManager/SearchDataManager.swift
@@ -6,19 +6,21 @@
 //
 
 import Foundation
+import Alamofire
+
 class SearchDataManager {
-//    func got(_ viewController: PresentMoaVC) {
-//        AF.request("\(Constant.BASE_URL)/keepin/all?title=keyword", method: .get, parameters: nil, headers: Constant.HEADER)
-//            .validate(statusCode: 200..<500)
-//            .validate(contentType: ["application/json"])
-//            .responseDecodable(of: PresentMoaHomeResponse.self) { response in
-//                switch response.result {
-//                case .success(let response):
-//                    viewController.serverData = response.data
-//                    viewController.didSuccessGot(message: response.message!)
-//                case .failure(let error):
-//                    print(error.localizedDescription)
-//                }
-//            }
-//    }
+    func got(_ viewController: SearchVC) {
+        AF.request("\(Constant.BASE_URL)/keepin/all?title=", method: .get, parameters: nil, headers: Constant.HEADER)
+            .validate(statusCode: 200..<500)
+            .validate(contentType: ["application/json"])
+            .responseDecodable(of: PresentMoaHomeResponse.self) { response in 
+                switch response.result {
+                case .success(let response):
+                    viewController.serverData = response.data
+                    viewController.didSuccessSearch(message: response.message!)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+    }
 }
