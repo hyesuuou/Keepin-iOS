b42244fd20a4d60eb019151fec1f3f8287ae9aff
eunyoung <eun970923@naver.com>
2021-07-12 15:20:39 +0900

    [feat] #66 키핀하기 새친구등록 수정

--- Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusFriend/KeepinPlusFriendVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusFriend/KeepinPlusFriendVC.swift
@@ -12,7 +12,8 @@ class KeepinPlusFriendVC: UIViewController {
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var lineView: UIView!
     @IBOutlet weak var textField: UITextField!
-    
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var warnLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,34 @@ class KeepinPlusFriendVC: UIViewController {
         mainLabel.textColor = UIColor.keepinBlack
         mainLabel.font = UIFont.GmarketSansTTF(.medium, size: 16)
         
+        doneButton.tintColor = .keepinGray3
+        doneButton.titleLabel?.font = .NotoSans(.bold, size: 16)
+        
         lineView.backgroundColor = .keepinGray3
         
         textField.attributedPlaceholder = NSAttributedString(string: "등록할 이름을 입력해 주세요.(최대 5자)", attributes: [NSAttributedString.Key.foregroundColor: UIColor.keepinGray3])
+        textField.addTarget(self, action: #selector(textFieldChange(_sender:)), for: .editingChanged)
+        setWarnLabel(label: warnLabel)
         
-
+        self.navigationController?.navigationBar.isHidden = true
     }
+    
+    @objc func textFieldChange(_sender: Any?){
+        if textField.text != ""{
+            doneButton.tintColor = .keepinGreen
+        }
+        //if textField.text
+    }
+    
+    func setWarnLabel(label:UILabel){
+        label.isHidden = true
+        label.font = UIFont.NotoSans(.regular, size: 14)
+        label.textColor = .salmon
+    }
+
+    @IBAction func toBack(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 
 }
