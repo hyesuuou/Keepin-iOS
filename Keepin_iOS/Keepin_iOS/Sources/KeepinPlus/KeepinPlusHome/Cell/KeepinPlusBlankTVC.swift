76a860c67ba0d056d2f813cdaf7057752ab67c26
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-12 14:52:41 +0900

    [feat] #79 파일추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/Cell/KeepinPlusBlankTVC.swift
@@ -0,0 +1,23 @@
+//
+//  KeepinPlusBlankTVC.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/12.
+//
+
+import UIKit
+
+class KeepinPlusBlankTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
