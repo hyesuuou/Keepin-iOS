5e28dd17cfeeccb76745bee5a8ab2f724f0f0f31
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-12 19:00:16 +0900

    [feat] #89 홈화면 서버연결 datamanager, model 생성

---
+++ Keepin_iOS/Keepin_iOS/Sources/Home/DataManager/HomeDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  HomeDataManager.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/12.
+//
+
+import Foundation
---
+++ Keepin_iOS/Keepin_iOS/Sources/Home/DataModel/HomeResponse.swift
@@ -0,0 +1,8 @@
+//
+//  HomeResponse.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/12.
+//
+
+import Foundation
