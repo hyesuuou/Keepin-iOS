10d19a42ff80e14913d7c78b87b090e4cc57c9c6
eunyoung <eun970923@naver.com>
2021-07-14 15:14:50 +0900

    [feat] 마이페이지 메인뷰 네비게이션바 파일추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageHome/Cell/NavigationBarCVC.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarCVC.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/14.
+//
+
+import UIKit
+
+class NavigationBarCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
