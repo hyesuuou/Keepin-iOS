8a4ba6d25a0b64675602ffdc6544220abb36bcd9
eunyoung <eun970923@naver.com>
2021-07-14 11:46:20 +0900

    [feat] #116 마이페이지 친구상세 파일추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageDetail/DataManager/MyPageDetailDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageDetailDataManager.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/14.
+//
+
+import Foundation
---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageDetail/DataModel/MyPageDetailResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageDetailResponse.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/14.
+//
+
+import Foundation
