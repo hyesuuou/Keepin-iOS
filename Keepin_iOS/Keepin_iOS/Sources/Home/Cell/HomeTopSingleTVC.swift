672fa5d8b480cecd841e4d988098d07d5b98691e
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-15 11:34:52 +0900

    [feat] #162 se2 하단(하나있는거) 파일추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/Home/Cell/HomeTopSingleTVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTopSingleTVC.swift
+//  Keepin_iOS
+//
+//  Created by 김혜수 on 2021/07/15.
+//
+
+import UIKit
+
+class HomeTopSingleTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
