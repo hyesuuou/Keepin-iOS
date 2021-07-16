96147908b6c566b0182143c39c6a344f4c9d7fa2
Yi Joon Choi <69078056+dlwns33@users.noreply.github.com>
2021-07-16 16:57:26 +0900

    [fix] 토큰 바꿈

--- Keepin_iOS/Keepin_iOS/Configuration/Secret/Token.swift
+++ Keepin_iOS/Keepin_iOS/Configuration/Secret/Token.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct Token{
-    static var jwt : String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZWRjNDJjNzRjNjkyNDk5OGFmMGNlOSIsImVtYWlsIjoiaU9TQG5hdmVyLmNvbSIsImlhdCI6MTYyNjE5OTc5MSwiZXhwIjoxNjI2ODA0NTkxfQ.yUq9xM3hA6jomifblNspncgk6-JDz4SefvqzKF8XTZc"
+    static var jwt : String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZWRjNDJjNzRjNjkyNDk5OGFmMGNlOSIsImZjbSI6ImN2a21qUzJhVGtyZHFIcmd1cWRsTzQ6QVBBOTFiRzVTT1RLUEJjX1pfRUw1X2FRZEtsWFBGMVk1LVVqdm84Z0ZZVm4zaThRLS1ybEZmcnJ1SW9jNDFxcXk3TlpjWGNQVVNYbzdvR2JoQThIdE9wYWFiSThJU2JobUhrV1gwYnRWSlZoRkFKa0hyYk9ia2NUV0o4MjlyVDhqdXZUdkJELWl6WkMiLCJpYXQiOjE2MjY0MjE2ODMsImV4cCI6MTYyNzAyNjQ4M30.HpNuY1_IcRT2u-PhlWHJehdsXhEYOV8OWOLsdi6QuBk"
     static var refreshToken : String = ""
     static var name : String = ""
 }
