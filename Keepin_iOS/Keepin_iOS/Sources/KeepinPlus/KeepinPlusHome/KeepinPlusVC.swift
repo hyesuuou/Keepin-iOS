f527edad937bdfaccbdf9969f2bf4b711cbedd80
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-16 23:55:38 +0900

    [feat] #218 이미지 전 아이디 값 받아오기 성공

--- Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/KeepinPlusVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/KeepinPlus/KeepinPlusHome/KeepinPlusVC.swift
@@ -17,6 +17,7 @@ class KeepinPlusVC: UIViewController {
     var blankHeight : Int = 0
     var selectfriendTextField : String = ""
     let imagePicker = UIImagePickerController()
+    var addId : String = ""
     
     
     @IBOutlet weak var backButton: UIButton!
@@ -48,6 +49,10 @@ class KeepinPlusVC: UIViewController {
 
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         
+    }
+    @IBAction func keepinButtonClicked(_ sender: Any) {
+        
+        KeepinPlusHomeDataManager().postKeepin(KeepinPlusHomeRequest(title: "가장 달콤했던 생일 선물", taken: true, date: "2021-06-07", category: ["생일", "축하"], record: "뽀민이 정말 앙큼하다. 나 몰래 케이크 주문해놓고 얼레벌레 들고 등장했다 >,< 귀여워!! 꽃 너무 예뻐서 드라이플라워로 간직할 거당. 케이크 너무 맛있었다. 보민이 생일날엔 더 맛있는 거 사줘야지!!", friendIdx: ["60ed9e98e51ad110481cd9d7"]), viewController: self)
     }
     
     @objc func loadList(_ notification : NSNotification)
