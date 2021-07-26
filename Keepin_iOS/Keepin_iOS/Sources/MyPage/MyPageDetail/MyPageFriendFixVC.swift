3f02162cf9c2e97bd60c3c2f9ad0aabec171ded5
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-07-26 23:43:10 +0900

    [fix] #238 친구상세 네비게이션 바 수정

--- Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageDetail/MyPageFriendFixVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageDetail/MyPageFriendFixVC.swift
@@ -12,47 +12,64 @@ class MyPageFriendFixVC: UIViewController {
     @IBOutlet weak var friendLabel: UILabel!
     @IBOutlet weak var textView: UIView!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    var friendName: String = ""
+    var id: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setStyle()
-        setNavigationBar()
+        
+        /*
+        NotificationCenter.default.addObserver(self, selector: #selector(load), name: NSNotification.Name("load2"), object: nil)
+        print(friendName,"성공성공")*/
+        
     }
-    
+    /*
+    @objc func load(_ notification : NSNotification)
+    {
+        let data = notification.object as? String ?? ""
+        self.friendName = data
+        print("이 함수가 잘 실행되나요?")
+    }*/
+
     func setStyle(){
         friendLabel.text = "친구의 이름을 입력해 주세요."
         friendLabel.textColor = .keepinBlack
         friendLabel.font  = UIFont.GmarketSansTTF(.medium, size: 16)
 
         textView.backgroundColor = .keepinBlack
+        doneButton.tintColor = .keepinGray3
+        doneButton.titleLabel?.font = UIFont.NotoSans(.bold, size: 16)
+        
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        textField.text = friendName
     }
     
-    func setNavigationBar(){
-        let backButton: UIButton = UIButton(type: UIButton.ButtonType.custom)
-        backButton.setImage(UIImage(named: "icBack"),for: UIControl.State.normal)
-        backButton.addTarget(self, action: #selector(toBack), for: UIControl.Event.touchUpInside)
-        backButton.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
-        
-        
-        let registerButton: UIButton = UIButton(type: UIButton.ButtonType.custom)
-        registerButton.setTitle("등록하기", for: UIControl.State.normal)
-        registerButton.setTitleColor(.keepinGreen, for: UIControl.State.normal)
-        registerButton.titleLabel?.font = UIFont.NotoSans(.bold, size: 16)
-        registerButton.frame = CGRect(x: 0, y: 0, width: 24, height: 49)
-        
-        let backbarButton = UIBarButtonItem(customView: backButton)
-        self.navigationItem.leftBarButtonItem = backbarButton
-        
-        let registerbarButton = UIBarButtonItem(customView: registerButton)
-        self.navigationItem.rightBarButtonItem = registerbarButton
-        
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = false
+    @objc func textFieldDidChange(_ sender: Any){
+        if textField.text != ""{
+            doneButton.tintColor = .keepinGreen
+        }
     }
     
-    @objc func toBack(){
-        self.navigationController?.popViewController(animated: true)
+    
+    @IBAction func toBack(_ sender: Any) {
+            self.navigationController?.popViewController(animated: true)
     }
+    
+    
+    @IBAction func doneButton(_ sender: Any) {
+        let request = MyPageFixRequest(name: textField.text!)
+        MyPageDetailDataManager().fixFriendName(id, modified: request, viewController: self)
+    }
+    
+}
 
+extension MyPageFriendFixVC{
+    func didsuccessFixFriendName(message: String){
+        print(message)
+    }
 }
