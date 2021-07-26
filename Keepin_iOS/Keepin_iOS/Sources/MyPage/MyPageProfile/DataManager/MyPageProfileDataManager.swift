db8afe0ab1f3d25b0a0a6a0e46f3196e0035f546
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-07-27 00:23:22 +0900

    [feat] 프로필 편집 이름수정 서버통신 파일 추가

---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageProfile/DataManager/MyPageProfileDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageProfileDataManager.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/27.
+//
+
+import Foundation
---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageProfile/DataModel/MyPageProfileNameRequest.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageProfileNameRequest.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/27.
+//
+
+import Foundation
