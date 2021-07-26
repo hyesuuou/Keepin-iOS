c0cdcce42035f6c0950674b3261df323a97563f5
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-07-27 00:44:58 +0900

    [feat] #243 프로필 편집 이름수정 서버통신

--- Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageProfile/DataModel/MyPageProfileNameRequest.swift
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageProfile/DataModel/MyPageProfileNameRequest.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct MyPageProfileNameRequest: Codable{
+    let name : String
+}
---
+++ Keepin_iOS/Keepin_iOS/Sources/MyPage/MyPageProfile/DataModel/MyPageProfileNameResponse.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageProfileNameResponse.swift
+//  Keepin_iOS
+//
+//  Created by 이은영 on 2021/07/27.
+//
+
+import Foundation
+
+struct MyPageProfileNameResponse: Codable {
+    let status: Int
+    let message: String
+}
+
