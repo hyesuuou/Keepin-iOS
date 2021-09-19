a19493d9950e2b2d65aad376a7c4d6cb8bbe5a7e
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-09-20 04:30:11 +0900

    [#86] 홈화면 랜덤 웰컴메시지 적용

--- Keepin_iOS/Keepin_iOS/Sources/Home/Cell/HomeTopTVC.swift
+++ Keepin_iOS/Keepin_iOS/Sources/Home/Cell/HomeTopTVC.swift
@@ -11,7 +11,15 @@ class HomeTopTVC: UITableViewCell {
     
     public static let identifier = "HomeTopTVC"
     
-    let username : String = "\(Token.name)"
+    let username: String = "\(Token.name)"
+    let welcomeMessage: [String] = ["직접 준 선물도 키핀해 보세요.",
+                                    "화면을 아래로 당겨볼까요?",
+                                    "감동받은 메시지도 키핀하세요.",
+                                    "오늘도 선물 같은 하루!",
+                                    "감사한 순간을 공유해 주세요!",
+                                    "선물은 미리미리 골라야죠!",
+                                    "지금 키핀하러 가볼까요?",
+                                    "예전에 받은 그 선물, 키핀해요!"]
     
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
@@ -32,20 +40,9 @@ class HomeTopTVC: UITableViewCell {
     }
     
     func setUI(){
-        
-        // welcomeLabel UI
-        welcomeLabel.text = "\(username)님\n키핀은 아름다워요"
-        welcomeLabel.font = UIFont.GmarketSansTTF(.medium, size: 22)
-        let attributedStr = NSMutableAttributedString(string: welcomeLabel.text!)
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = 8
-        attributedStr.addAttribute(.foregroundColor, value: UIColor.keepinGreen , range: (welcomeLabel.text! as NSString).range(of: "\(username)"))
-        attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedStr.length))
-        welcomeLabel.attributedText = attributedStr
-    
         // 랜덤이미지, 랜덤타이틀 UI
         //randomImageView.image = UIImage(named: "imgEmptyEyes")
-        messageLabel.text = ""
+        //messageLabel.text = ""
         messageLabel.font = UIFont.GmarketSansTTF(.medium, size: 14)
         messageLabel.textColor = .keepinGray5
         
@@ -53,4 +50,17 @@ class HomeTopTVC: UITableViewCell {
         emptyLabel.textColor = .keepinGray3
         emptyBgView.backgroundColor = .keepinGray1
     }
+    
+    // MARK: - Welcome Label 셋팅
+    func setData(){
+        welcomeLabel.text = "\(username)님\n\(welcomeMessage.randomElement() ?? "")"
+        welcomeLabel.font = UIFont.GmarketSansTTF(.medium, size: 22)
+        let attributedStr = NSMutableAttributedString(string: welcomeLabel.text!)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 8
+        attributedStr.addAttribute(.foregroundColor, value: UIColor.keepinGreen , range: (welcomeLabel.text! as NSString).range(of: "\(username)"))
+        attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedStr.length))
+        welcomeLabel.attributedText = attributedStr
+    }
+
 }
